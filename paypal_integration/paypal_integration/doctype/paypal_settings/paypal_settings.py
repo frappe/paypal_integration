812e03fb9e38440f7080cc358fb58be00fa9be11
Nabin Hait <nabinhait@gmail.com>
2016-02-01 15:45:46 +0530

    [fix] create payment gateway record after installation only if erpnext installed

--- paypal_integration/paypal_integration/doctype/paypal_settings/paypal_settings.py
+++ paypal_integration/paypal_integration/doctype/paypal_settings/paypal_settings.py
@@ -9,6 +9,5 @@ from paypal_integration.after_install import create_gateway_account
 
 class PayPalSettings(Document):
 	def on_update(self):
-		create_gateway_account()
-		
-		
+		if frappe.db.exists("DocType", "Payment Gateway"):
+			create_gateway_account()

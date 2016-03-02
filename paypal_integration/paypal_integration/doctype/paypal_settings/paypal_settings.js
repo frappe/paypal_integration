9e6019666b80c9e6addbeb71e8303d567987d194
Rushabh Mehta <rmehta@gmail.com>
2016-03-02 11:28:11 +0530

    [fix] link

--- paypal_integration/paypal_integration/doctype/paypal_settings/paypal_settings.js
+++ paypal_integration/paypal_integration/doctype/paypal_settings/paypal_settings.js
@@ -4,7 +4,7 @@ frappe.ui.form.on("PayPal Settings", {
 			frappe.set_route("List", "Paypal Express Payment");
 		});
 		frm.add_custom_button(__("Payment Gateway Accounts"), function() {
-			frappe.route_options = {"gateway": "PayPal"};
+			frappe.route_options = {"payment_gateway": "PayPal"};
 			frappe.set_route("List", "Payment Gateway Account");
 		});
 	}

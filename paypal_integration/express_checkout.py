fead2a24c0de9a2d5fceab8be837550edd0973ba
Anand Doshi <anand@erpnext.com>
2016-03-17 17:33:38 +0530

    [fix] Added payment success and improved UX

--- paypal_integration/express_checkout.py
+++ paypal_integration/express_checkout.py
@@ -22,10 +22,10 @@ class PaypalException(Exception): pass
 @frappe.whitelist(allow_guest=True, xss_safe=True)
 def set_express_checkout(amount, currency="USD", data=None):
 	validate_transaction_currency(currency)
-	
+
 	if not isinstance(data, basestring):
 		data = json.dumps(data or "{}")
-	
+
 	response = execute_set_express_checkout(amount, currency)
 
 	paypal_settings = get_paypal_settings()
@@ -48,7 +48,7 @@ def set_express_checkout(amount, currency="USD", data=None):
 		if data.get("doctype") and  data.get("docname"):
 			paypal_express_payment.reference_doctype = data.get("doctype")
 			paypal_express_payment.reference_docname = data.get("docname")
-		
+
 	paypal_express_payment.insert(ignore_permissions = True)
 	frappe.db.commit()
 
@@ -68,7 +68,7 @@ def execute_set_express_checkout(amount, currency):
 
 	params = urlencode(params) + \
 		"&returnUrl={0}&cancelUrl={1}".format(return_url, get_url("/paypal-express-cancel"))
-		
+
 	return get_api_response(params.encode("utf-8"))
 
 @frappe.whitelist(allow_guest=True, xss_safe=True)
@@ -86,11 +86,11 @@ def get_express_checkout_details(token):
 	paypal_express_payment.status = "Verified"
 	paypal_express_payment.save(ignore_permissions=True)
 	frappe.db.commit()
-	
+
 	frappe.local.response["type"] = "redirect"
 	frappe.local.response["location"] = get_url( \
 		"/api/method/paypal_integration.express_checkout.confirm_payment?token="+paypal_express_payment.token)
-	
+
 @frappe.whitelist(allow_guest=True, xss_safe=True)
 def confirm_payment(token):
 	paypal_express_payment = frappe.get_doc("Paypal Express Payment", token)
@@ -108,17 +108,27 @@ def confirm_payment(token):
 	try:
 		get_api_response(params)
 
+	except PaypalException, e:
+		frappe.db.rollback()
+		frappe.get_doc({
+			"doctype": "Paypal Log",
+			"error": "{e}\n\n{traceback}".format(e=e, traceback=frappe.get_traceback()),
+			"params": frappe.as_json(params)
+		}).insert(ignore_permissions=True)
+		frappe.db.commit()
+
+		frappe.local.response["type"] = "redirect"
+		frappe.local.response["location"] = get_url("/paypal-express-failed")
+
+	else:
 		paypal_express_payment = frappe.get_doc("Paypal Express Payment", token)
 		paypal_express_payment.status = "Completed"
 		paypal_express_payment.save(ignore_permissions=True)
-				
 		trigger_ref_doc(paypal_express_payment, "set_as_paid")
-		
 		frappe.db.commit()
-		
-	except PaypalException:
+
 		frappe.local.response["type"] = "redirect"
-		frappe.local.response["location"] = get_url("/paypal-express-cancel")
+		frappe.local.response["location"] = get_url("/paypal-express-success")
 
 def get_paypal_params():
 	paypal_settings = get_paypal_settings()
@@ -129,7 +139,7 @@ def get_paypal_params():
 			"SIGNATURE": paypal_settings.signature,
 			"VERSION": "98"
 		}
-		
+
 	else :
 		return {
 			"USER": frappe.conf.paypal_username,
@@ -152,10 +162,17 @@ def get_api_response(params):
 	if response.get("ACK")[0]=="Success":
 		return response
 	else:
-		raise PaypalException
+		raise PaypalException(response)
 
 def get_paypal_settings():
-	return frappe.get_doc("PayPal Settings")
+	paypal_settings = frappe.get_doc("PayPal Settings")
+
+	# update from site_config.json
+	for key in ("paypal_sandbox", "paypal_username", "paypal_password", "paypal_signature"):
+		if key in frappe.local.conf:
+			paypal_settings.set(key, frappe.local.conf[key])
+
+	return paypal_settings
 
 def validate_transaction_currency(currency):
 	if currency not in ["AUD", "BRL", "CAD", "CZK", "DKK", "EUR", "HKD", "HUF", "ILS", "JPY", "MYR", "MXN",
@@ -165,11 +182,11 @@ def validate_transaction_currency(currency):
 def trigger_ref_doc(paypal_express_payment, method):
 	page_mapper = {"Orders": "orders", "Invoices": "invoices", "My Account": "me"}
 	if paypal_express_payment.reference_doctype and paypal_express_payment.reference_docname:
-		
-		ref_doc = frappe.get_doc(paypal_express_payment.reference_doctype, 
+
+		ref_doc = frappe.get_doc(paypal_express_payment.reference_doctype,
 			paypal_express_payment.reference_docname)
 		ref_doc.run_method(method)
-		
+
 		if method != "set_as_cancelled":
 			frappe.local.response["type"] = "redirect"
 			shopping_cart_settings = frappe.get_doc("Shopping Cart Settings")

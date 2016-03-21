10717410722ce551ef23de3368806f9bace42297
Rushabh Mehta <rmehta@gmail.com>
2016-03-21 16:46:02 +0530

    [fix] patch

--- paypal_integration/patches/redo_install.py
+++ paypal_integration/patches/redo_install.py
@@ -1,6 +1,8 @@
 import frappe
 
 def execute():
-	from paypal_integration.after_install import create_gateway_account, create_payment_gateway
+	from paypal_integration.paypal_integration.doctype.paypal_settings import (create_gateway_account,
+	create_payment_gateway)
+
 	create_payment_gateway()
 	create_gateway_account()
